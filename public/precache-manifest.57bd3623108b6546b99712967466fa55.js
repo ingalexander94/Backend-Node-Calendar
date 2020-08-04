65135d57869c6d5feb93576d526715005d446fe8
Alexander <alexanderpenaloza3@gmail.com>
2020-08-04 14:09:17 -0500

    Frontend con REACT añadido a la carpeta public

---
+++ public/precache-manifest.57bd3623108b6546b99712967466fa55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9029eb9d3937c013241caf09c5a62466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9a3b792ec0e02b7db6",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "0eba7085de92eba5a688",
+    "url": "/static/css/main.7ae9ff40.chunk.css"
+  },
+  {
+    "revision": "da9a3b792ec0e02b7db6",
+    "url": "/static/js/2.2c6f2c45.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.2c6f2c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eba7085de92eba5a688",
+    "url": "/static/js/main.732a5b32.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
